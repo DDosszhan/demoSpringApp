7b8f0ab24a99e61be53e244e549a0282646e34c1
doszhan <doszhankonysuly@gmail.com>
2024-07-17 09:50:20 +0500

    First commit

---
+++ src/main/java/org/example/HelloController.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Hello, Spring Boot!";
+    }
+
+
+    @GetMapping("/main")
+    public String main() {
+        return "<h1>MAIN PAGE</h1>";
+    }
+}
